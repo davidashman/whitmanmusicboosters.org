9c746a9609c1aaa21fac7a75e6ae13dea99e541a
David Ashman <david@ashman.com>
2025-10-01 08:53:23 -0400

    Use logo with a background

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,9 @@ import Image from 'next/image'
 export default function Header({ campaign }: {campaign?: string}) {
   const title = campaign ? campaign.replace(/-/g, ' ') : '';
   return (
-    <div className="text-center mb-12 py-10 bg-[#1a1a1a] text-4xl font-bold text-[#7198C8] text-center font-baro">
-      {title}
-      {!campaign && <Image src="/logo.png" alt="Whitman Music Boosters" width={500} height={184} className="mx-auto my-2 h-28 w-auto" />}
+    <div className="text-center mb-12 py-7 bg-[#1a1a1a] text-4xl font-bold text-[#7198C8] text-center font-baro">
+      {campaign && <p className="py-3">{title}</p>}
+      {!campaign && <Image src="/logo.png" alt="Whitman Music Boosters" width={720} height={300} className="mx-auto h-38 w-auto" />}
     </div>
   )
 }
