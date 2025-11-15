4467652ee6718bb6fcb2c5ae39861f872c7c30ef
David Ashman <david@ashman.com>
2025-11-15 18:07:29 -0500

    Make home page adjustable

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -4,11 +4,13 @@ import Header from '@/components/Header'
 import SupportMessage from '@/components/SupportMessage'
 
 export default function Home() {
+  const campaign = process.env.NEXT_PUBLIC_HOME_CAMPAIGN || '2025-2026-general-fundraising';
+
   return (
     <main className="min-h-screen bg-white mx-auto">
       <Header />
       <SupportMessage />
-      <DonorboxWidget campaign="fall-fundraising-campaign-2025-2026"/>
+      <DonorboxWidget campaign={campaign}/>
       <Copyright />
     </main>
   )
