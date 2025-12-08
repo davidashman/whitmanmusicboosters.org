65f19074b23eb0d5933e28b8810f1bd43fb116b3
David Ashman <david@ashman.com>
2025-12-07 21:03:19 -0500

    Use title from env if set

--- src/app/banner/page.tsx
+++ src/app/banner/page.tsx
@@ -5,11 +5,13 @@ import SupportMessage from '@/components/SupportMessage'
 
 export default function Banner() {
   const campaign = process.env.NEXT_PUBLIC_BANNER_CAMPAIGN || '2025-2026-general-fundraising';
+  const title = process.env.NEXT_PUBLIC_BANNER_TITLE || '2025-2026 General Fundraising';
+
   return (
     <main className="min-h-screen bg-white mx-auto">
       <Header />
       <SupportMessage />
-      <DonorboxWidget campaign={campaign} />
+      <DonorboxWidget campaign={campaign} title={title} />
       <Copyright />
     </main>
   )
--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -5,12 +5,13 @@ import SupportMessage from '@/components/SupportMessage'
 
 export default function Home() {
   const campaign = process.env.NEXT_PUBLIC_HOME_CAMPAIGN || '2025-2026-general-fundraising';
+  const title = process.env.NEXT_PUBLIC_HOME_TITLE || '2025-2026 General Fundraising';
 
   return (
     <main className="min-h-screen bg-white mx-auto">
       <Header />
       <SupportMessage />
-      <DonorboxWidget campaign={campaign} />
+      <DonorboxWidget campaign={campaign} title={title} />
       <Copyright />
     </main>
   )
