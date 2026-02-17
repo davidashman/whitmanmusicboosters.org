d1754f56021c4ddce602006eedbd908af2e0a9ef
David Ashman <david@ashman.com>
2026-02-17 18:26:23 -0500

    Add code to the form

--- src/components/GiveButterWidget.tsx
+++ src/components/GiveButterWidget.tsx
@@ -33,7 +33,7 @@ export default function GiveButterWidget({
         </p>
       )}
       <givebutter-widget id={id}></givebutter-widget>
-      <p className="text-xs text-center  text-[#ccc]">{id}</p>
+      <p className="text-xs text-center  text-[#fff]">{id}</p>
     </div>
   );
 }
