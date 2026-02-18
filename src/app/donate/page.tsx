184c73162fd9f11cf1a14afb340aaa0964a11f8b
David Ashman <david@ashman.com>
2026-02-18 10:31:31 -0500

    feat: use parseWidgetConfig in donate page

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -4,16 +4,16 @@ import Copyright from "@/components/Copyright";
 import Header from "@/components/Header";
 import SupportMessage from "@/components/SupportMessage";
 import GiveButterWidget from "@/components/GiveButterWidget";
+import { parseWidgetConfig } from "@/lib/parseWidgetConfig";
 
 export default function Home() {
-  const widget = process.env.NEXT_PUBLIC_HOME_WIDGET || "jbOGOL";
-  const title = process.env.NEXT_PUBLIC_HOME_TITLE;
+  const config = parseWidgetConfig(process.env.NEXT_PUBLIC_HOME_WIDGET, "jbOGOL");
 
   return (
     <main className="min-h-screen bg-white mx-auto">
       <Header />
       <SupportMessage />
-      <GiveButterWidget id={widget} title={title} />
+      <GiveButterWidget id={config.id} title={config.title} />
       <Copyright />
     </main>
   );
